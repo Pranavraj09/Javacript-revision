99262957049e3cb2ae4252fe57e625d027132318
Pranavraj09 <pranavraj0909@gmail.com>
2025-06-25 13:44:13 +0530

    done

---
+++ chai aur code JS/01-variables.js
@@ -0,0 +1,37 @@
+const name = "Pranav raj";
+let school ="KSVCEM";
+var id="Abc123";
+department ="cse";
+let rollno;
+school ="kunwar satyavira college of engineering and management";
+console.log(name);
+console.table([name,school,id,department,rollno]);
+
+
+
+
+// const accountID = 14453//const keyword does not change
+// let accountEmail ="abc121@gmail.com"  
+// var accountPassword="Delhi"
+// accountCity = "bijnor" /* some time can be used mostly they cannot be used*/
+
+// // accountID=12345 not allowed
+// accountEmail="bbcdw8520@gmail.com"
+// accountPassword="jaipur"
+// accountCity="Delhi"
+// console.log(accountEmail)
+// console.table([accountEmail,accountID,accountPassword,accountCity]);
+
+
+
+/*
+prefer not to use var
+bacause of issue in block scope and functional scope
+Let:let have Block Scope
+Variables declared with let must be Declared before use.
+var always have Global Scope.
+Variables declared with the var keyword can NOT have block scope:
+const cannot be Redeclared
+Variables defined with const cannot be Reassigned
+Variables defined with const have Block Scope
+*/
---
+++ chai aur code JS/02-datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello")//we are using nodejs, not browser
+
+// js can be avoid space
+
+console.log(3+7 )
+//code readability  should be high
+
+console.log('Hitesh')
+
+let name="hitesh"
+let age=18
+let isLoggedIn= false
+
+//number => 2 to 53
+//bigint can be used for a large number that are used
+//string =>""
+//boolean=>true/false
+//null=> standalone value
+//undefined =>they can not be assign the value
+//symbol=>unique
+
+
+//object
+
+console.log(typeof"hitesh");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+//console.log(type of): they can be used for for clearify the value should be whose type of datatype 
+//null can be test the type of it can obtain the answer those has the object.
+
+
+
