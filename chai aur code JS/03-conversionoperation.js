a674efe4cb769b04a919cc70cf693e2eecbb1df9
Pranavraj09 <pranavraj0909@gmail.com>
2025-06-26 22:21:00 +0530

    done today

--- chai aur code JS/03-conversionoperation.js
+++ chai aur code JS/03-conversionoperation.js
@@ -54,7 +54,7 @@ console.log(typeof islogedin);
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " Pranav"
 
 let str3 = str1 + str2
 // console.log(str3);
@@ -63,6 +63,7 @@ let str3 = str1 + str2
 // console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + 3 + "4");
+// not used these type
 
 //  console.log(((3 + 5) * 5 ) % 3);
  
@@ -70,6 +71,10 @@ let str3 = str1 + str2
 
 // console.log(+true);
 // console.log(+"");
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num1);
+
 
 let gameCounter = 100
 gameCounter++;
---
+++ chai aur code JS/datatypessummary.js
@@ -0,0 +1,18 @@
+// there are two types of datatypes
+// 1) Primitive data type
+// 2) Non-Primitive data type
+
+/*
+1) Primitive data type    (call by value)
+7 types of  Primitive data type
+String,Number,boolean,null,undefined,Symbol,BigInt
+
+2) Non-Primitive data type(reference type)
+Arrays,Objects,functions
+*/
+// Javascript is a dynamic language.
+
+const id=Symbol('123456');
+const anotherid=Symbol('12345');
+console.log([anotherid,id]);
+
