d6b5e5ed2249e5e8a32330ed25539861636b9007
Pranavraj09 <pranavraj0909@gmail.com>
2025-07-01 16:07:38 +0530

    done

--- chai aur code JS/datatypessummary.js
+++ chai aur code JS/datatypessummary.js
@@ -7,12 +7,23 @@
 7 types of  Primitive data type
 String,Number,boolean,null,undefined,Symbol,BigInt
 
-2) Non-Primitive data type(reference type)
-Arrays,Objects,functions
+const score=100; // Number
+const name='John'; // String
+const isActive=true; // Boolean
+const user=null; // Null
+let age; // Undefined
+
+
 */
 // Javascript is a dynamic language.
 
-const id=Symbol('123456');
-const anotherid=Symbol('12345');
-console.log([anotherid,id]);
+// const id=Symbol('123456');
+// const anotherid=Symbol('123456');
+// console.log([anotherid === id]);
+
+// const bigNumber =1020230303948840488n; bigInt
+
+
+// 2) Non-Primitive data type(reference type)
+// Arrays,Objects,functions
 
